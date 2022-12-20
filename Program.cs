de82ca591dd99c7eb1dcd40e809c17d325239aa7
Oleg Iraklionov <irakliov@mail.ru>
2022-12-20 17:24:28 +0300

    Case2 is ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и 
+// выдает максимальное из них
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+
+int maximum = 0;
+if (number1>number2)
+    {
+         maximum = number1;
+    }
+else
+    {
+         maximum = number2;
+    }
+if (number3>maximum)
+    {
+         maximum = number3;
+    }
+Console.WriteLine(maximum);
